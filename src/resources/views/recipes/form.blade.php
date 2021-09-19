6ebe8dcb8bb307b2440962446e4c5abfba8c7967
Satoh-K37 <skohcode0385@gmail.com>
2021-09-20 00:39:35 +0900

    feat:レシピ更新機能の実装 close #43

--- src/resources/views/recipes/form.blade.php
+++ src/resources/views/recipes/form.blade.php
@@ -1,48 +1,48 @@
 @csrf
 <div class="md-form">
-  <label>タイトル</label>
-  <input type="text" name="recipe_title" class="form-control" required value="{{ old('recipe_title') }}">
+  <label>レシピタイトル</label>
+  <input type="text" name="recipe_title" class="form-control" required value="{{ $recipe->recipe_title ?? old('recipe_title') }}">
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="content" required class="form-control" rows="16" placeholder="本文を">{{ old('content') }}</textarea>
+  <textarea name="content" required class="form-control" rows="16" placeholder="本文">{{ $recipe->content ?? old('content') }}</textarea>
 </div>
 
 <div class="md-form">
   <p>材料:</p>
   
-  <input value="数値を入力してください" type="text" name="serving" class="form-control" required value="{{ old('serving') }}">
-  <input type="text" name="ingredient" class="form-control" required value="{{ old('ingredient') }}">
+  <input placeholder="数値を入力してください" type="text" name="serving" class="form-control" required value="{{ $recipe->serving ?? old('serving') }}">
+  <input type="text" name="ingredient" class="form-control" required value="{{ $recipe->ingredient ?? old('ingredient') }}">
 </div>
 <div class="md-form">
   <label>調味料</label>
-  <input type="text" name="seasoning" class="form-control" required value="{{ old('seasoning') }}">
+  <input type="text" name="seasoning" class="form-control" required value="{{ $recipe->seasoning ?? old('seasoning') }}">
 </div>
 <div class="md-form">
   <label>Step1</label>
-  <input type="text" name="step_content" class="form-control" required value="{{ old('step_content') }}">
+  <input type="text" name="step_content" class="form-control" required value="{{ $recipe->step_content ?? old('step_content') }}">
 </div>
 <div class="md-form">
   <label>Step2</label>
-  <input type="text" name="step_content2" class="form-control" required value="{{ old('step_content2') }}">
+  <input type="text" name="step_content2" class="form-control" required value="{{ $recipe->step_content2 ?? old('step_content2') }}">
 </div>
 <div class="md-form">
   <label>Step3</label>
-  <input type="text" name="step_content3" class="form-control" required value="{{ old('step_content3') }}">
+  <input type="text" name="step_content3" class="form-control" required value="{{ $recipe->step_content3 ?? old('step_content3') }}">
 </div>
 <div class="md-form">
   <label>Step4</label>
-  <input type="text" name="step_content4" class="form-control" required value="{{ old('step_content4') }}">
+  <input type="text" name="step_content4" class="form-control" required value="{{ $recipe->step_content4 ?? old('step_content4') }}">
 </div>
 <div class="md-form">
   <label>Step5</label>
-  <input type="text" name="step_content5" class="form-control" required value="{{ old('step_content5') }}">
+  <input type="text" name="step_content5" class="form-control" required value="{{ $recipe->step_content5 ?? old('step_content5') }}">
 </div>
 <div class="md-form">
   <label>Step6</label>
-  <input type="text" name="step_content6" class="form-control" required value="{{ old('step_content6') }}">
+  <input type="text" name="step_content6" class="form-control" required value="{{ $recipe->step_content6 ?? old('step_content6') }}">
 </div>
 <div class="md-form">
   <label>コツ・ポイント</label>
-  <input type="text" name="cooking_point" class="form-control" required value="{{ old('cooking_point') }}">
+  <input type="text" name="cooking_point" class="form-control" required value="{{ $recipe->cooking_point ?? old('cooking_point') }}">
 </div>
