ddd25e503467309446049ee6c5bfb496fc3b7f3f
Satoh-K37 <skohcode0385@gmail.com>
2021-10-22 22:32:57 +0900

    feat:検索結果用のviewを追加

--- src/resources/views/recipes/search.blade.php
+++ src/resources/views/recipes/search.blade.php
@@ -6,12 +6,12 @@
       <!--検索フォーム-->
       <div class="row">
         <div class="col-sm">
-          <form method="GET" action="{{ route('recipes.search')}}">
+          <form method="GET" action="{{ route('recipes.searchResult')}}">
             <div class="form-group row">
               <label class="col-sm-2 col-form-label">キーワード</label>
               <!--入力-->
               <div class="col-sm-5">
-                <input type="text" class="form-control" name="searchWord"  placeholder="キーワードを入力してください">
+                <input type="search" class="form-control" name="searchWord" value="{{ $searchWord }}" placeholder="キーワードを入力してください">
               </div>
               <div class="col-sm-auto">
                 <button type="submit" class="btn btn-primary ">検索</button>
@@ -21,7 +21,7 @@
             <div class="form-group row">
               <label class="col-sm-2">レシピカテゴリ</label>
               <div class="col-sm-3">
-                <select name="category_id" class="form-control" value=>
+                <select name="category_id" class="form-control" value="{{ $category_id }}">
                   @foreach($categories as $id => $category_name)
                   <option value="{{ $id }}">
                     {{ $category_name }}
