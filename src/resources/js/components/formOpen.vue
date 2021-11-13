dabb663ad5668356efb0166fd13e43eeb0e601e2
Satoh-K37 <skohcode0385@gmail.com>
2021-11-13 23:06:40 +0900

    feat:レシピ投稿の際にフォームをクリックすると隠されたフォームが表示させるための準備

---
+++ src/resources/js/components/formOpen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="photo-form">
+    <h2 class="title">Submit a recipe</h2>
+    <form class="form">
+      <input class="form__item" type="file">
+      <div class="form__button">
+        <button type="submit" class="button button--inverse">submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
