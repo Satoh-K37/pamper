b033b8364adafb4a475321e8eeec5ccba9dcad00
Satoh-K37 <skohcode0385@gmail.com>
2022-03-15 07:07:18 +0900

    [feat]S3の画像のアップロード処理を変更

--- src/app/Http/Controllers/RecipeController.php
+++ src/app/Http/Controllers/RecipeController.php
@@ -168,7 +168,8 @@ class RecipeController extends Controller
           // ユニークなファイル名をimage_pathカラムに代入
           $form['image_path'] = $filename_to_store;
           // S3にリサイズした画像をオリジナルのファイル名でアップロードする
-          Storage::disk('s3')->putFile('public/images/'. $filename_to_store, $resized_image);
+          // Storage::disk('s3')->putFile('public/images/'. $filename_to_store, $resized_image);
+          Storage::disk('s3')->put('public/images/'. $filename_to_store, $resized_image);
           
           // $path = Storage::disk('s3')->put('public/images/', $resized_image, 'public');
           // dd($path);
