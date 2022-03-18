39f2a02eb68fea48b275dd7bd18b700e50a9a9ad
Satoh-K37 <skohcode0385@gmail.com>
2022-03-18 22:46:30 +0900

    [feat]ユーザーアイコンの更新処理のロジックとアイコンの表示を変更

--- src/resources/views/recipes/index_card.blade.php
+++ src/resources/views/recipes/index_card.blade.php
@@ -32,7 +32,7 @@
                 @endif
               @else
                 @if($recipe->user->profile_image !== NULL)
-                  <img src="{{ Storage::disk('s3')->get("$recipe->user->profile_image") }}" class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
+                  <img src="{{ $recipe->user->profile_image }}" class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
                 @else
                   <img src="{{ Storage::disk('s3')->url("default_icon.png") }}" class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
                 @endif
