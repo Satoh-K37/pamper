afb7a663a998db6997392df7742779c433cdd8c9
Satoh-K37 <skohcode0385@gmail.com>
2022-05-11 16:46:27 +0900

    [feat]Userモデルを使えるように修正

--- src/app/User.php
+++ src/app/User.php
@@ -7,8 +7,12 @@ use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
+
 
 class User extends Authenticatable
 {
