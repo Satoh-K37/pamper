cb4e91bfb9947477a66501939bf13209b865fcaa
Satoh-K37 <skohcode0385@gmail.com>
2022-05-25 13:41:36 +0900

    [fix]フォロワー一覧、フォロー一覧のアイコンが自分のものが表示されるのを修正

--- src/resources/views/users/person.blade.php
+++ src/resources/views/users/person.blade.php
@@ -2,25 +2,25 @@
   <div class="card-body">
     <div class="d-flex flex-row">
       <a href="{{ route('users.show', ['name' => $person->name]) }}" class="text-dark">
-        <!-- <i class="fas fa-user-circle fa-3x"></i> -->
         @if(app()->isLocal() || app()->runningUnitTests())
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-            @if($user->profile_image !== NULL)
-              <img src='/storage/icons/{{$user->profile_image}}' class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
+          <a href="{{ route('users.show', ['name' => $person->name]) }}" class="text-dark">
+            @if($person->profile_image !== NULL)
+              <img src='/storage/icons/{{$person->profile_image}}' class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
             @else
               <img src='/storage/default_icon.png' class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
             @endif
           </a>
         @else
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-            @if($user->profile_image !== NULL)
-              <img src="{{ $user->profile_image }}" class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
+          <a href="{{ route('users.show', ['name' => $person->name]) }}" class="text-dark">
+            @if($person->profile_image !== NULL)
+              <img src="{{ $person->profile_image }}" class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
             @else
               <img src="{{ Storage::disk('s3')->url("default_icon.png") }}" class="rounded-circle" style="object-fit: cover; width: 50px; height: 50px;">
             @endif
           </a>
         @endif
       </a>
+
       @if( Auth::id() !== $person->id )
         <follow-button
           class="ml-auto"
