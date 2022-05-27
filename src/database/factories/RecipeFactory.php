76a38dd591c9508b7f7078e995c30ca57deac50d
Satoh-K37 <skohcode0385@gmail.com>
2022-05-27 17:07:06 +0900

    [feat]RecipeFactory.phpで型の不整合を修正

--- src/database/factories/RecipeFactory.php
+++ src/database/factories/RecipeFactory.php
@@ -11,17 +11,9 @@ $factory->define(Recipe::class, function (Faker $faker) {
       'recipe_title' => $faker->text(50),
       'content' => $faker->text(500),
       'serving' => $faker->randomDigit(5),
-      'ingredient' => $faker->string(255),
-      'seasoning' => $faker->string(255),
-      'step_content' => $faker->string(255),
-      'step_content2' => $faker->string(255),
-      'step_content3' => $faker->string(255),
-      'step_content4' => $faker->string(255),
-      'step_content5' => $faker->string(255),
-      'step_content6' => $faker->string(255),
       'cooking_time' => $faker->randomDigit(5),
       'user_id' => function() {
-          return factory(App/User::class);
+          return factory(User::class);
       }
     ];
 });
